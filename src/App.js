a4bfd8b8cfb5bfae6832615bd62c426e9eb5da80
Increation-on <maxim.d@gmail.com>
2020-03-12 21:30:36 +0300

    lesson 18

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Profile from './components/Profile';
+import Dialogs from './components/Dialogs/Dialogs.jsx';
 
 const App = () => {
   return (
     <div className='app-wrapper'>
       <Header />
       <Navbar />
-      <Profile />
+      <div class="app-wrapper-content">
+        <Dialogs />
+      </div>
+      {/* <Profile /> */}
     </div>);
 }
 
