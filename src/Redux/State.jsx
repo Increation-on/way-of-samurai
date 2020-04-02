c89288a3fadb1a4c7d48b85beb8751f51ea87dbd
Increation-on <maxim.d132213@gmail.com>
2020-04-02 21:17:43 +0300

    lesson 37

--- src/Redux/State.jsx
+++ src/Redux/State.jsx
@@ -1,54 +1,55 @@
-let rerenderEntireTree =()=>{
-}
 
-let state = {
-    profilePage:{
-        postData: [
-            { message: "message1", id: 1, likesCount: 15 },
-            { message: "message2", id: 2, likesCount: 20 },
-            { message: "message3", id: 3, likesCount: 26 }
-        ],
-        newPostText: ""
+let store = {
+    _state: {
+        profilePage: {
+            postData: [
+                { message: "message1", id: 1, likesCount: 15 },
+                { message: "message2", id: 2, likesCount: 20 },
+                { message: "message3", id: 3, likesCount: 26 }
+            ],
+            newPostText: ""
+        },
+        dialogsPage: {
+            dialogsData: [
+                { name: "Rexxar", id: 1 },
+                { name: "Thrall", id: 2 },
+                { name: "Jaina", id: 3 },
+                { name: "Rexxar", id: 4 },
+                { name: "Uther", id: 5 }
+            ],
+
+            messagesData: [
+                { message: "hey", id: 1 },
+                { message: "yoyoyo", id: 2 },
+                { message: "hello", id: 3 }
+            ]
+        }
     },
-    dialogsPage:{
-        dialogsData: [
-            { name: "Rexxar", id: 1 },
-            { name: "Thrall", id: 2 },
-            { name: "Jaina", id: 3 },
-            { name: "Rexxar", id: 4 },
-            { name: "Uther", id: 5 }
-        ],
-      
-        messagesData: [
-            { message: "hey", id: 1 },
-            { message: "yoyoyo", id: 2 },
-            { message: "hello", id: 3 }
-        ]
-    },  
+    _callSubscriber() {
+        console.log("changed store")
+    },
+    getState() {
+        return  this._state
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.postData.push(newPost);
+        this._state.profilePage.newPostText = "";
+        this._callSubscriber(this._state);
+    },
+    
+    updateNewPostText(newPostText) {
+        this._state.profilePage.newPostText = newPostText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    }
 }
 
-window.state = state;
-
-export const addPost = ()=>{
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-    state.profilePage.postData.push(newPost);
-    state.profilePage.newPostText ="";
-    rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newPostText)=>{
-    state.profilePage.newPostText = newPostText;
-    rerenderEntireTree(state);
-} 
-
-export const subscribe = (observer) =>{
-    rerenderEntireTree = observer;
-}
-
-
-export default state;
+export default store;
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state from './Redux/State';
+import store from './Redux/State';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import {addPost, updateNewPostText, subscribe} from './Redux/State';
 
  let rerenderEntireTree = (state)=>{
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost} 
-            updateNewPostText = {updateNewPostText}/>
+            <App state={state} addPost={store.addPost.bind(store)} 
+            updateNewPostText = {store.updateNewPostText.bind(store)}/>
         </BrowserRouter>,
         document.getElementById('root'));
 }
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree)
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree)
 
 
 
