5a53b7ea6e97d7f9700ce8fc0dedd9086b27f1ee
Increation-on <maxim.d132213@gmail.com>
2020-05-05 20:48:23 +0300

    lesson 52

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import styles from './Users.module.css'
+import * as axios from 'axios'
+import userPhoto from '../../Assets/images/user.jpg'
 
 const Users = (props) => {
     if(props.users.length === 0 ){
-        props.setUsers(
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then((response)=>{
+            props.setUsers(response.data.items);
+        })
+
+       /*  props.setUsers(
             [
                 {id:1, fullName:"Jaina", status:"mage", location: {city: "Dalaran", country: "Northrend"}, followed: true, photoURL:'https://d2hhj3gz5jljkm.cloudfront.net/wallpapers2/012/793/074/944/original/450px_.jpg'},
                 {id:2, fullName:"Arthas", status:"paladin", location: {city: "Dalaran", country: "Northrend"}, followed: false, photoURL:'https://d2hhj3gz5jljkm.cloudfront.net/wallpapers2/012/793/074/944/original/450px_.jpg'},
                 {id:3, fullName:"Thrall", status:"shaman", location: {city: "Dalaran", country: "Northrend"}, followed: true, photoURL:'https://d2hhj3gz5jljkm.cloudfront.net/wallpapers2/012/793/074/944/original/450px_.jpg'},
                 {id:4, fullName:"Deathwing", status:"dragon", location: {city: "Dalaran", country: "Northrend"}, followed: false, photoURL:'https://d2hhj3gz5jljkm.cloudfront.net/wallpapers2/012/793/074/944/original/450px_.jpg'}
             ]
-        )
+        ) */
     }
 
     return (
@@ -18,7 +25,7 @@ const Users = (props) => {
             return <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoURL} alt=""  className={styles.ava}/>
+                        <img src={ u.photos.small !== null ? u.photos.small : userPhoto } alt="image"  className={styles.ava}/>
                     </div>
                     <div>
                         {u.followed ?
@@ -27,12 +34,12 @@ const Users = (props) => {
                     </div>
                 </span>
                 <span>
-                    <div>{u.fullName}</div>
+                    <div>{u.name}</div>
                     <div>{u.status}</div>
                 </span>
                 <span>
-                    <div>{u.location.country}</div>
-                    <div>{u.location.city}</div>
+                    <div>{"u.location.country"}</div>
+                    <div>{"u.location.city"}</div>
                 </span>
             </div>
         })}
